60ff1cee954514e7d31b595f98fe81a062eeeb33
卢兴超 <luxingchao@bimwinner.com>
2020-02-16 23:53:15 +0800

    add contact in page.php

--- wp-content/themes/giga-store/page.php
+++ wp-content/themes/giga-store/page.php
@@ -10,6 +10,23 @@ if ( class_exists( 'WooCommerce' ) && get_theme_mod( 'breadcrumbs-check', 1 ) !=
 <?php get_template_part( 'content', 'page' ); ?>
 <!-- end content container -->
 
+
+<div id="contactUs" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Request a Free Quote</h4>
+      </div>
+      <div class="modal-body">
+		<?php echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php
 
 get_template_part( 'template-part', 'footernav' );
