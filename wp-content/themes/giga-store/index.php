4ad1427b38e140b97dffc11315eb438d944094cf
卢兴超 <luxingchao@bimwinner.com>
2020-02-16 23:21:53 +0800

    change footer position

--- wp-content/themes/giga-store/index.php
+++ wp-content/themes/giga-store/index.php
@@ -33,6 +33,22 @@ if ( class_exists( 'WooCommerce' ) && get_theme_mod( 'breadcrumbs-check', 1 ) !=
 </div>
 <!-- end content container -->
 
+<div id="contactUs" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Request a Free Quote</h4>
+      </div>
+      <div class="modal-body">
+		<?php echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php get_template_part( 'template-part', 'footernav' ); ?>
 
 <?php get_footer(); ?>
