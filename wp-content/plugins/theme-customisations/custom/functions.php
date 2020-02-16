77d7c7d2b6ac97cb43fcc087327e9b5db55c5efc
卢兴超 <luxingchao@bimwinner.com>
2020-02-17 00:15:43 +0800

    update

--- wp-content/plugins/theme-customisations/custom/functions.php
+++ wp-content/plugins/theme-customisations/custom/functions.php
@@ -242,18 +242,23 @@ if ( ! function_exists( 'woocommerce_template_loop_product_thumbnail_custom' ) )
 		echo '<div class="product-img-wrap br-2-white">'. woocommerce_get_product_thumbnail() .'</div>';
 	}
 }
-echo '
+
+?>
+
 <div id="contactUs" class="modal fade" role="dialog">
-<div class="modal-dialog">
-	<!-- Modal content-->
-	<div class="modal-content">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal">&times;</button>
-			<h4 class="modal-title">Request a Free Quote</h4>
-		</div>
-		<div class="modal-body">'
-	. echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ) .
-		'</div>
-	</div>
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Request a Free Quote</h4>
+      </div>
+      <div class="modal-body">
+		<?php echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ); ?>
+      </div>
+    </div>
+  </div>
 </div>
-</div>'
+
+<?php
