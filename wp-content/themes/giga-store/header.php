b9d1e7025df3fc706145e8297227eeeefe8cec7c
卢兴超 <luxingchao@bimwinner.com>
2020-02-17 00:04:03 +0800

    add in function.php

--- wp-content/themes/giga-store/header.php
+++ wp-content/themes/giga-store/header.php
@@ -9,21 +9,6 @@
 	</head>
 	<body id="blog" <?php body_class(); ?>>
 
-		<div id="contactUs" class="modal fade" role="dialog">
-			<div class="modal-dialog">
-				<!-- Modal content-->
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal">&times;</button>
-						<h4 class="modal-title">Request a Free Quote</h4>
-					</div>
-					<div class="modal-body">
-				<?php echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<?php get_template_part( 'template-part', 'topnav' ); ?>
 
 		<?php get_template_part( 'template-part', 'head' ); ?>
