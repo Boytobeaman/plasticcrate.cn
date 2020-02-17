e57252da25c5fbfae7dbc27466945ce0d11f68ef
卢兴超 <luxingchao@bimwinner.com>
2020-02-17 10:08:47 +0800

    aa

--- wp-content/plugins/theme-customisations/custom/functions.php
+++ wp-content/plugins/theme-customisations/custom/functions.php
@@ -249,7 +249,21 @@ add_action( 'wp_footer', 'add_contact_modal',99 );
 
 function add_contact_modal() {
 	?>
-	echo '<h4 id="contactUs">test contact</h4>'
+	<div id="contactUs" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Request a Free Quote</h4>
+				</div>
+				<div class="modal-body">
+			<?php echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 <?php
 };
 	
