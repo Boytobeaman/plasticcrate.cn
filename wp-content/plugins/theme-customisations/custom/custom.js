1c90926072ce67c053b9e2351c585acb16bb7e7f
卢兴超 <luxingchao@bimwinner.com>
2020-02-25 23:55:55 +0800

    add ;

--- wp-content/plugins/theme-customisations/custom/custom.js
+++ wp-content/plugins/theme-customisations/custom/custom.js
@@ -1,38 +1,4 @@
-// jQuery(document).ready(function($){
-// 	// Custom jQuery goes here
-// 	$("#masthead nav.navbar").addClass("navbar-dark");
-// 	$('#productURL').val(window.document.location.href);
-// 	if ($('.product-model .value').length > 0) {
-// 		$("#ProductModel").val($('.product-model .value').html());
-// 	}
-
-// 	$(document).on("click", ".product a.woocommerce-LoopProduct-link,.product-inquiry", function (e) {
-// 		if ($(e.target).hasClass("product-model")) {
-// 			e.preventDefault();
-// 		} else if ($(e.target).hasClass("product-cat-inquiry")) {
-// 			e.preventDefault();
-// 			var productModel = $(this).find(".product-model").html();
-// 			$("#ProductModel").val(productModel);
-// 			jQuery("html,body").animate({
-// 				scrollTop: jQuery("#text-6").offset().top
-// 			}, 800, function () {
-// 				$("input[name='Quantity']").focus();
-// 			});
-// 			// $("#contactUs").modal();
-// 		} else if ($(e.target).hasClass("product-inquiry")) {
-// 			$("#ProductModel").val($('.product-model .value').html());
-			
-// 			jQuery("html,body").animate({
-// 				scrollTop: jQuery("#text-6").offset().top
-// 			}, 800,function () {
-// 				$("input[name='Quantity']").focus();
-// 			});
-// 			// $("#contactUs").modal();
-// 		}
-// 	})
-// });
-
-jQuery(function($){
+;jQuery(function($){
 	$(document).on("click",".product a.woocommerce-LoopProduct-link,.product-inquiry", function(e){
 		if($(e.target).hasClass("product-model")){
 			e.preventDefault();
@@ -50,5 +16,4 @@ jQuery(function($){
         var url = window.document.location.href;
         $('#productURL').val(url);
     });
-
 })
